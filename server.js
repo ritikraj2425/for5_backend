22a97679aab8681e5fc00ddfc1e6318fdcd3103f
ritikraj2425 <rajritik2425@gmail.com>
2024-09-11 18:47:52 +0530

    Add all backend files

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config({ path: './env' })
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const mongoURI = "mongodb+srv://rajritik2425:qH8UD3y3ztRMZ2Kj@for5-db.o229c.mongodb.net/for5?retryWrites=true&w=majority";
+const port = 5000;
+
+
+mongoose.connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+const questionSchema = new mongoose.Schema({
+    questionName: String,
+    question: String,
+    difficulty: String,
+    year: String,
+    repetitive: Boolean,
+    subject: String,
+    topic: String,
+    questionType: String,
+    weightage: Number,
+    tags: [String],
+    options: [Object]
+}, { collection: 'Questions' });
+
+
+const Question = mongoose.model('Question', questionSchema);
+
+// Endpoint to get all questions
+app.get('/api/questions', async (req, res) => {
+    try {
+        const questions = await Question.find({});
+        console.log(questions);
+        res.json(questions);
+    } catch (error) {
+        console.error('Error fetching questions:', error);
+        res.status(500).json({ message: 'Error fetching questions' });
+    }
+});
+
+
+app.get('/api/questions/:id', async (req, res) => {
+    try {
+        const question = await Question.findById(req.params.id);
+        if (question) {
+            res.json(question);
+        } else {
+            res.status(404).json({ message: 'Question not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Endpoint to filter questions by subject
+app.get('/api/questions/filter/:subject', async (req, res) => {
+    try {
+        const questions = await Question.find({ subject: req.params.subject });
+        if (questions && questions.length > 0) {
+            res.json(questions);
+        } else {
+            res.status(404).json({ message: 'Questions not found for this subject' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Endpoint to test connection
+app.get('/api/test', async (req, res) => {
+    try {
+        const count = await Question.countDocuments({});
+        res.json({ message: `Number of documents: ${count}` });
+    } catch (error) {
+        res.status(500).json({ message: 'Error testing connection' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
