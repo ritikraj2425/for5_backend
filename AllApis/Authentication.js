49b6b27ca59edf8d8a0bbc410d4c3331eed3e30e
Ritik Raj <149488602+ritikraj2425@users.noreply.github.com>
2024-09-22 14:49:39 +0530

    Update Authentication.js 4

--- AllApis/Authentication.js
+++ AllApis/Authentication.js
@@ -10,104 +10,104 @@ const Users  = mongoose.model('Users',userDetailsSchema);
 const AuthenticationRoutes = {
     path : "",
     routes : [
-        // {
-        //     method : "post",
-        //     path : "/signup",
-        //     handler : async (req,res)=>{
-        //         const{name,username,email,password} = req.body;
-        //         if(!name || !username || !email || !password){
-        //             res.status(400).send({
-        //                 message:"all fields are required"
-        //             });
-        //             return;
-        //         }
-        //         const checkUsernameAndEmail  = await Users.findOne({username: username,email:email});
-        //         if(checkUsernameAndEmail){
-        //             res.status(400).send({message:"username and email already exist"});
-        //             return;
-        //         }
-        //         const checkUsername  = await Users.findOne({username: username});
-        //         if(checkUsername){
-        //             res.status(400).send({message:"username already exist"});
-        //             return;
-        //         }
-        //         const checkEmail  = await Users.findOne({username: username});
-        //         if(checkEmail){
-        //             res.status(400).send({message:"email is already registered"});
-        //             return;
-        //         }
-        //         try{
-        //             const hash = await bcrypt.hash(password,10);
-        //             const user = new Users({
-        //                 ...req.body,
-        //                 password : hash
-        //             });
-        //             await user.save();
-        //             const payload = {name,username,email};
-        //             const token = jwt.sign(payload,jwtSecret,{expiresIn:"24h"});
-        //             const refresh_token = jwt.sign(payload,refreshSecret, {expiresIn:"7d"});
-        //             res.status(200).send({
-        //                 message:"success",
-        //                 token:token,
-        //                 refresh_token:refresh_token
-        //             });
-        //             return;
+        {
+            method : "post",
+            path : "/signup",
+            handler : async (req,res)=>{
+                const{name,username,email,password} = req.body;
+                if(!name || !username || !email || !password){
+                    res.status(400).send({
+                        message:"all fields are required"
+                    });
+                    return;
+                }
+                const checkUsernameAndEmail  = await Users.findOne({username: username,email:email});
+                if(checkUsernameAndEmail){
+                    res.status(400).send({message:"username and email already exist"});
+                    return;
+                }
+                const checkUsername  = await Users.findOne({username: username});
+                if(checkUsername){
+                    res.status(400).send({message:"username already exist"});
+                    return;
+                }
+                const checkEmail  = await Users.findOne({username: username});
+                if(checkEmail){
+                    res.status(400).send({message:"email is already registered"});
+                    return;
+                }
+                try{
+                    const hash = await bcrypt.hash(password,10);
+                    const user = new Users({
+                        ...req.body,
+                        password : hash
+                    });
+                    await user.save();
+                    const payload = {name,username,email};
+                    const token = jwt.sign(payload,jwtSecret,{expiresIn:"24h"});
+                    const refresh_token = jwt.sign(payload,refreshSecret, {expiresIn:"7d"});
+                    res.status(200).send({
+                        message:"success",
+                        token:token,
+                        refresh_token:refresh_token
+                    });
+                    return;
 
-        //         }
-        //         catch(e){
-        //             res.status(500).send({message:"something went wrong"});
-        //         }
-        //     }
-        // },
-        // {
-        //     method : "post",
-        //     path : "/login",
-        //     handler : async (req,res)=>{
-        //         const{username, email, password} = req.body;
-        //         if(!password || (!email && !username)){
-        //             res.status(400).send({message:"all fields are required"});
-        //             return;
-        //         }
-        //         try{
-        //             let user;
-        //             if(!username){
-        //                 user = await Users.findOne({email:email});
-        //                 if(!user){
-        //                     res.status(404).send({message:"User with this email does not exist"});
-        //                     return;
-        //                 }
-        //             }
-        //             else if(!email){
-        //                 user = await Users.findOne({username:username});
-        //                 if(!user){
-        //                     res.status(404).send({message:"User with this username does not exist"});
-        //                     return;
-        //                 }
-        //             }
-        //             const checkPassword = await bcrypt.compare(password, user.password);
-        //             if(!checkPassword){
-        //                 res.status(400).send({message:"Wrong password"});
-        //                 return;
-        //             }
-        //             const payload = {
-        //                 name:user.name,
-        //                 username:user.username,
-        //                 email:user.email
-        //             };
-        //             const token = jwt.sign(payload,jwtSecret,{expiresIn:"24h"});
-        //             const refresh_token = jwt.sign(payload,refreshSecret, {expiresIn:"7d"});
-        //             res.status(200).send({
-        //                 message:"login successfull",
-        //                 token:token,
-        //                 refresh_token:refresh_token
-        //             });
-        //             return;
-        //         }
-        //         catch(e){
-        //             res.status(500).send({message:"something went wrong"});
-        //         }
-        //     }
-        // }
+                }
+                catch(e){
+                    res.status(500).send({message:"something went wrong"});
+                }
+            }
+        },
+        {
+            method : "post",
+            path : "/login",
+            handler : async (req,res)=>{
+                const{username, email, password} = req.body;
+                if(!password || (!email && !username)){
+                    res.status(400).send({message:"all fields are required"});
+                    return;
+                }
+                try{
+                    let user;
+                    if(!username){
+                        user = await Users.findOne({email:email});
+                        if(!user){
+                            res.status(404).send({message:"User with this email does not exist"});
+                            return;
+                        }
+                    }
+                    else if(!email){
+                        user = await Users.findOne({username:username});
+                        if(!user){
+                            res.status(404).send({message:"User with this username does not exist"});
+                            return;
+                        }
+                    }
+                    const checkPassword = await bcrypt.compare(password, user.password);
+                    if(!checkPassword){
+                        res.status(400).send({message:"Wrong password"});
+                        return;
+                    }
+                    const payload = {
+                        name:user.name,
+                        username:user.username,
+                        email:user.email
+                    };
+                    const token = jwt.sign(payload,jwtSecret,{expiresIn:"24h"});
+                    const refresh_token = jwt.sign(payload,refreshSecret, {expiresIn:"7d"});
+                    res.status(200).send({
+                        message:"login successfull",
+                        token:token,
+                        refresh_token:refresh_token
+                    });
+                    return;
+                }
+                catch(e){
+                    res.status(500).send({message:"something went wrong"});
+                }
+            }
+        }
         
     ]
 }
